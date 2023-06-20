89d6613d208bc103cad74d888947e2825b311c1c
Khulan Ulziibat <khulan.ulz@gmail.com>
2023-06-20 09:09:08 -0400

    remove rounding from printing & add training and test sets for feed_forward_nn

--- and_gate.py
+++ and_gate.py
@@ -1,14 +1,16 @@
 from argparse import ArgumentParser
 import math
 
+
 def sigmoid_func(x):
     return 1 / (1 + math.e ** -x)
+    
     
 def boolean_and(x1, x2):
     weight1 = 20
     weight2 = 20
     bias = -30
-    return sigmoid_func(weight1 * x1 + weight2 * x2 + bias)
+    return round(sigmoid_func(weight1 * x1 + weight2 * x2 + bias))
     
 
 def main():
@@ -20,7 +22,7 @@ def main():
     x1 = args.x1
     x2 = args.x2
     
-    print(f"{x1} and {x2} => {round(boolean_and(x1, x2))}")
+    print(f"{x1} and {x2} => {boolean_and(x1, x2)}")
     
     
 if __name__ == "__main__":
